50dd479d24121d9af6dd090fba931f30b82b24ac
Stevensauro <31481885+Stevensauro@users.noreply.github.com>
2022-12-23 10:37:01 -0600

    cambios

--- JS/encriptador.js
+++ JS/encriptador.js
@@ -1,20 +1,43 @@
 function encriptar(texto = document.getElementById("textoString").value){
     
+    let htmlResultado = document.getElementById("contenedorResultado")
+
+    let htmlResultadoChildren = document.getElementById("contenedorResultado").children
+
     texto = texto.replace(/(\r\n|\n|\r)/gm," ")
     const regex = /^[a-z ]+$/gm //Regular Expression
     
-    let result = regex.test(texto)
-    
-   
-    if( result == true && texto != 0 ){
+    let resultado = regex.test(texto)
+
+    if( resultado == true && texto != 0 ){
+
       textoEncriptado = texto.replace(/e/g, "enter")
-      document.getElementById("resultado").innerHTML = textoEncriptado
+
+      if (htmlResultadoChildren.length < 2){
+
+        htmlResultado.innerHTML += `<p id="resultado">${textoEncriptado}</p>`
+
+      } else{
+        htmlResultado.children[1].innerHTML = textoEncriptado
+      }
+
     } else if (result == false && Boolean(texto) == true){
+    
       alert("Error - Utiliza solamente letras minusculas!")
+    
     } else{
+    
       alert("Error - Necesitas escribir algo!")
+    
     }
-    console.log(result)  
-    console.log(Boolean(texto))
+
 }
+
+
+
+
+/*
+    console.log(Boolean(texto))
+    console.log(resultado)
+*/
 
